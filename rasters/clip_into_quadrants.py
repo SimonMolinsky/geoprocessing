fbad459f2c35605d3096d2405636120fb664c920
szymon-datalions <s.molinski@datalions.eu>
2019-05-17 18:24:43 +0200

    raster clipping update

--- rasters/clip_into_quadrants.py
+++ rasters/clip_into_quadrants.py
@@ -1,19 +1,12 @@
-"""Script for cutting raster image into quadrants with georefernce.
-Developed by: Szymon Moliński (s.molinski@datalions.eu)
-Date of last review: 12.05.2019
-Reviewed by: Szymon Moliński (s.molinski@datalions.eu)"""
-
-
-import affine
-import numpy as np
-import rasterio as rio
-
-import warnings
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
-
-
 class ImageForClipModel:
+    """Class for clipping big raster images into smaller parts. Class initialized with image address.
+    Class has only one method with two modes of work:
+    
+    1 - clipped data is saved,
+    2 - clipped data is stored in the list clipped_images.
+    
+    Default mode is (2). It is recommend to use it only during the development and debugging with small images.
+    """
 
     # Get band, band shape, dtype, crs and transform values
     def __init__(self, image_address):
@@ -24,9 +17,41 @@ class ImageForClipModel:
         self.band_shape = self.band.shape
         self.band_dtype = self.band.dtype
         self.clipped_images = []
+        self.clipped_addresses = []
 
     # Function for clipping band
-    def clip_raster(self, height, width, buffer, save=False, prefix='clipped_band_', pass_empty=False):
+    def clip_raster(self, height, width, buffer=0, save_mode=False, prefix='clipped_band_', pass_empty=False):
+        """
+        Function clips and stores / saves raster data
+        
+        INPUT:
+        :param height: height of the clipped image in pixels.
+            If (height of the raster) % (height of clipped image) != 0 then one slice of the clipped tiles
+            will have height = (height of the raster) % (height of clipped image),
+        :param width: width of the clipped image in pixels,
+            If (width of the raster) % (width of clipped image) != 0 then one slice of the clipped tiles
+            will have width = (width of the raster) % (width of clipped image),
+        :param buffer: default = 0. 
+        If buffer is provided then clipped tiles overlaps by number of pixels provided in this
+            variable. As example: if width = 1000 and buffer is 10 then the first tile will be extracted from
+            columns 1 to 1000, the second tile from 991 to 1990. The formula is:
+            where i > 1
+            then start_column = 1000 * (i - 1) - (buffer * (i - 1)) + 1
+            end_column = start_column + 999
+        :param save_mode: default = False.
+        If save_mode is true then clipped tiles will be stored as the new raster images, else clipped tiles
+            are stored in the class attribute clipped_images (list),
+        :param prefix: default = 'clipped_band_'.
+        Prefix of the clipped data if it is saved as the static files.
+        :param pass_empty: default = False.
+        Method checks clipped data with np.mean function to prevent saving images which contain only zeros (usually
+        no data values). Optional.
+        
+        OUTPUT
+        :return clipped_addresses: if save_mode is True (list of strings with the clipped tiles paths)
+        or
+        :return clipped_images: if save_mode is False (list of clipped bands as numpy arrays).
+        """
         row_position = 0
         while row_position < self.band_shape[0]:
             col_position = 0
@@ -49,16 +74,15 @@ class ImageForClipModel:
                          self.band_dtype]
 
                 # Save or append into a set
-                if save:
+                if save_mode:
                     filename = prefix + 'x_' + str(col_position) + '_y_' + str(row_position) + '.tif'
                     with rio.open(filename, 'w', driver='GTiff', height=image[3],
                                   width=image[4], count=1, dtype=image[5],
                                   crs=image[1], transform=image[2]) as dst:
                         dst.write(image[0], 1)
-                    print('Image {} saved successfully'.format(filename))
+                    clipped_addresses.append(filename)
                 else:
                     self.clipped_images.append(clipped_image)
-                    print('Image {} added to set'.format('0'))
 
                 # Update column position
                 col_position = col_position + width - buffer
@@ -66,8 +90,9 @@ class ImageForClipModel:
             # Update row position
             row_position = row_position + height - buffer
 
-        if save:
-            print('Process ended with success')
-            return 0
+        if save_mode:
+            print('Tiles saved successfully')
+            return self.clipped_addresses
         else:
+            print('Tiles prepared successfully')
             return self.clipped_images
