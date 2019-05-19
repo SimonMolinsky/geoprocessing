3d7ecbb001ce3a63a67f2911c7fc18921c106120
Szymon Moliński <szymonmolinski@MacBook-Pro-Szymon.local>
2019-05-19 23:30:32 +0200

    check rasters fct

---
+++ rasters/check_rasters.py
@@ -0,0 +1,34 @@
+import os
+import numpy as np
+import rasterio as rio
+import matplotlib.pyplot as plt
+
+
+def read_images(folder_name, file_end):
+    file_list = os.listdir(folder_name)
+    channel_list = []
+    for f in file_list:
+        if f.endswith(file_end):
+            channel_list.append(folder_name + f)
+    channel_list.sort()
+    return channel_list
+
+
+def show_band(band, color_map='Set1', title=None):
+    band = band.astype(np.float)
+    band[band == 0] = np.nan
+    plt.figure(figsize=(11, 11))
+    image_layer = plt.imshow(band)
+    image_layer.set_cmap(color_map)
+    plt.colorbar()
+    if title is not None:
+        plt.title(title)
+    plt.show()
+
+
+if __name__ == '__main__':
+    images = read_images('ffi_output/', '.tiff')
+    for img in images:
+        with rio.open(img) as src:
+            image = src.read(1)
+            show_band(image, color_map='Set1', title=img)
